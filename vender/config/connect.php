327999ec4bad2c94b1b7ff4cfe4e1dc38db67e6e
individ52 <63917948+individ52@users.noreply.github.com>
2021-05-03 20:45:08 +0300

    no repeats

--- vender/config/connect.php
+++ vender/config/connect.php
@@ -1,6 +1,6 @@
 <?php 
     
-    $connect = mysqli_connect("localhost", "root", "", "tenniscontroller");
+    $connect = mysqli_connect("localhost", "root", "root", "tenniscontroller");
 
     if(!$connect) {
         die("error");
--- vender/create.php
+++ vender/create.php
@@ -17,9 +17,35 @@ $fotalFailForOne = $list["fotalFailForOne"];
 $fotalFailForTwo = $list["fotalFailForTwo"];
 $data            = $list["data"];
 echo "<pre>";
-mysqli_query($connect, "INSERT INTO `maindata` 
-(`id`, `playersName`, `globalScore`, `allTimeInGame`, `biggestRound`, `numOfFirst`, `roundTimeFast`, `roundTimeSlow`, `fotalFailForOne`, `fotalFailForTwo`, `lastGame`) VALUES 
-(NULL, '$playersName', '$dayScore', '$dayTime', '$biggestRound', '$firsts', '$roundTimeFast', '$roundTimeSlow', '$fotalFailForOne', '$fotalFailForTwo', '$data')");
+// получить данные из базы
+$result = mysqli_query($connect, "SELECT * FROM `maindata`");
+$maindata = [];
+$add_new;
+// print_r($maindata->num_rows);
+if ($result->num_rows > 0) {
+    $add_new= true;
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "push all Data<br>";
+        array_push($maindata, $row);
+    }
+  } else {
+      echo "first players<br>";
+    mysqli_query($connect,"INSERT INTO `maindata`(`id`, `playersName`, `globalScore`, `allTimeInGame`, `biggestRound`, `numOfFirst`, `roundTimeFast`, `roundTimeSlow`, `fotalFailForOne`, `fotalFailForTwo`, `lastGame`) VALUES  
+    (NULL, '$playersName', '$dayScore', '$dayTime', '$biggestRound', '$firsts', '$roundTimeFast', '$roundTimeSlow', '$fotalFailForOne', '$fotalFailForTwo', '$data')");
 
+  }
+// проверка существование пользователя
+foreach($maindata as $key) {
+    if($key["playersName"] === $playersName) {
+        echo "it is in base<br>";
+        $add_new = false;
+    }
+}
+if($add_new){
+    echo "add new players in data<br>";
+    mysqli_query($connect,"INSERT INTO `maindata`(`id`, `playersName`, `globalScore`, `allTimeInGame`, `biggestRound`, `numOfFirst`, `roundTimeFast`, `roundTimeSlow`, `fotalFailForOne`, `fotalFailForTwo`, `lastGame`) VALUES  
+    (NULL, '$playersName', '$dayScore', '$dayTime', '$biggestRound', '$firsts', '$roundTimeFast', '$roundTimeSlow', '$fotalFailForOne', '$fotalFailForTwo', '$data')");
+}
 
-?>  
+?>      
